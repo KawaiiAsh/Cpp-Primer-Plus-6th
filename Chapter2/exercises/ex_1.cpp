7e7a2bef8fe997d3ec9766064983b72b1c2142cf
KaibutsuSama <xinleroy@umich.edu>
2024-03-03 23:49:26 -0500

    update: code

--- Chapter2/exercises/ex_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string name = "Ash";
-    std::string address = "White House";
-
-    std::cout << "My name is " << name << ". My main address is " << address << std::endl;
-    return 0;
-}
